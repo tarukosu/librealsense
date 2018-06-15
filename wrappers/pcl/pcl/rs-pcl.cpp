a6a03ec815afbe172467d8ad9c36a0024310e676
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2018-06-15 13:42:20 +0900

    add socket communication

--- wrappers/pcl/pcl/rs-pcl.cpp
+++ wrappers/pcl/pcl/rs-pcl.cpp
@@ -1,8 +1,12 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
 
+
+
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include "../../../examples/example.hpp" // Include short list of convenience functions for rendering
+
+
 
 #include <pcl/point_types.h>
 #include <pcl/filters/passthrough.h>
@@ -13,6 +17,18 @@
 #include <pcl/visualization/cloud_viewer.h>
 
 #include <pcl/compression/octree_pointcloud_compression.h>
+
+
+// socket connection
+
+#include <stdio.h>
+// socket
+#define FD_SETSIZE 32
+#define BUFFER 512
+
+#include <windows.h>
+#include <winsock2.h>
+
 
 SYSTEMTIME operator-(const SYSTEMTIME& pSr, const SYSTEMTIME& pSl);
 
@@ -226,6 +242,43 @@ int main(int argc, char * argv[]) try
 	GetLocalTime(&endTime);
 //	int cumulativeDataLength = 0;
 
+
+	// socket connection
+	WSADATA wsaData;
+	SOCKET sock;
+	struct sockaddr_in addr;
+	struct timeval t_val = { 0, 1000 };
+	int select_ret;
+	char buf[BUFFER];
+	fd_set fds, readfds;
+	int accept_list[FD_SETSIZE];
+	int accept_num = 0;
+
+	// accept_list 初期化
+	for (int i = 0; i<FD_SETSIZE; i++) {
+		accept_list[i] = INVALID_SOCKET;
+	}
+
+	memset(&addr, 0, sizeof(struct sockaddr_in));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(48002);
+	addr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+	if (
+		(sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET ||
+		::bind(sock, (struct sockaddr *)&addr, sizeof(addr)) == SOCKET_ERROR ||
+		listen(sock, FD_SETSIZE) == SOCKET_ERROR
+		) {
+		printf("socket error\n");
+		return 1;
+	}
+	FD_ZERO(&readfds);
+	FD_SET(sock, &readfds);
+	// end socket connection
+
+
     while (app) // Application still alive?
     {
 		// Wait for the next set of frames from the camera
@@ -273,6 +326,10 @@ int main(int argc, char * argv[]) try
 		}
 #endif
 
+		if (cloud_filtered->size() == 0) {
+			continue;
+		}
+
 		// compress 
 		std::stringstream compressedData;
 		PointCloudEncoderRGB->encodePointCloud(cloud_filtered, compressedData);
@@ -317,6 +374,90 @@ int main(int argc, char * argv[]) try
 		//layers_rgb.push_back(cloud_filtered);
 		layers_rgb.push_back(decompressed_cloud);
 		draw_pointcloud_rgb(app, app_state, layers_rgb);
+
+
+
+		// socket connection
+
+		memcpy(&fds, &readfds, sizeof(fd_set));
+		select_ret = select(0, &fds, NULL, NULL, &t_val);
+		// timeoutでない場合
+		if (select_ret != 0) {
+			// 待ちうけソケットにデータがある
+			// 通信時にはacceptしたsocketを使うはずなので、待ちうけに来るものは必ずaccept対象
+			if (FD_ISSET(sock, &fds)) {
+				struct sockaddr_in client;
+				int len = sizeof(client);
+				int client_sock = accept(sock, (struct sockaddr *)&client, &len);
+				if (client_sock != INVALID_SOCKET) {
+					// 空いているところから登録
+					int i = 0;
+					while (i < FD_SETSIZE && accept_list[i] != INVALID_SOCKET) i++;
+					if (i != FD_SETSIZE) {
+						FD_SET(client_sock, &readfds);
+						accept_list[i] = client_sock;
+						printf("accept\n");
+					}
+					else {
+						printf("空きがありません\n");
+					}
+				}
+				else {
+					printf("accept error\n");
+				}
+			}
+
+			// 各ソケットの状況チェック
+			for (int i = 0; i<FD_SETSIZE; i++) {
+				if (accept_list[i] != -1 && FD_ISSET(accept_list[i], &fds)) {
+					int x;
+					memset(buf, 0, BUFFER);
+					x = recv(accept_list[i], buf, BUFFER, 0);
+
+					if (x > 0) {
+					//if (x >= 0) {
+						// 受信データ処理
+						buf[BUFFER - 1] = '\0';
+						printf("recv[%d][%d]: %s [status: %d]\n", i, accept_list[i], buf, x);
+
+						//const char *sendbuf = "Client: sending data test";
+						//const char *sendbuf = compressedDataString.c_str();
+						int message_size = compressedDataString.size() + 4;
+						char *sendbuf = new char[compressedDataString.size() + 4];
+						//sendbuf[compressedDataString.size()] = 0;
+						int data_size = compressedDataString.size();
+
+						printf("data size[%d]\n", data_size);
+
+						sendbuf[3] = (data_size >> 24) & 0xFF;
+						sendbuf[2] = (data_size >> 16) & 0xFF;
+						sendbuf[1] = (data_size >> 8) & 0xFF;
+						sendbuf[0] = data_size & 0xFF;
+
+						/*
+						sendbuf[0] = (data_size >> 24) & 0xFF;
+						sendbuf[1] = (data_size >> 16) & 0xFF;
+						sendbuf[2] = (data_size >> 8) & 0xFF;
+						sendbuf[3] = data_size & 0xFF;
+						*/
+
+						memcpy(&sendbuf[4], compressedDataString.c_str(), compressedDataString.size());
+						int result = send(accept_list[i], sendbuf, message_size, 0);
+						//int result = send(accept_list[i], sendbuf, strlen(sendbuf), 0);
+						//int result = send(accept_list[i], buf, strlen(buf), 0);
+						printf("result %d", result);
+
+					}
+					else {
+						// 通信異常（相手からいきなり切断されると常にここが呼び出されたので。）
+						printf("disconnect?[%d][%d]\n", i, accept_list[i]);
+						closesocket(accept_list[i]);
+						accept_list[i] = -1;
+					}
+				}
+			}
+		}
+
     }
 
     return EXIT_SUCCESS;
